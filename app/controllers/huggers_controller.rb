d14cfcd63fc80af3c4eef0b3517fdcbc10c4a6a3
Florent GONON <florent.gonon33@gmail.com>
2019-11-19 12:14:36 +0100

    update create index method huggers controller

--- app/controllers/huggers_controller.rb
+++ app/controllers/huggers_controller.rb
@@ -1,10 +1,10 @@
 class HuggersController < ApplicationController
   skip_before_action :authenticate_user!
   def index
+    @huggers = User.where(hugger: true)
   end
-  
+
   def show
     @hugger = Hugger.find(params[:id])
-
   end
 end
