8c8ce09a1a20f675e93b59d6e4dfaa223df452d7
xavier delarue <xdelarue33@gmail.com>
2019-11-20 15:21:09 +0100

    hugger bookings update edit ctr

--- app/controllers/hugger/bookings_controller.rb
+++ app/controllers/hugger/bookings_controller.rb
@@ -1,13 +1,22 @@
-class Hugger::BookingsController < ApplicationController
+class Hugger::BookingsController < ApplicationController  
   def edit
+    @bookings = current_user.bookings
   end
 
   def update
+    @booking = Booking.find(params[:id])
+    @booking.progress = params[:progress]    
+
+    if @booking.save
+      redirect_to edit_hugger_hug_booking_path
+    else
+      render :new
+    end
   end
 
   private
 
   def bookings_params
-    params.require(:booking).permit(:check_in, :check_out, :user_id, :hug_id)
+    params.require(:booking).permit(:progress)
   end
 end
