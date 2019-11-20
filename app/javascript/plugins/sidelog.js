244fbf22ec328419f95ad6c4a63d025c1fa1a524
KevinLamSeck <kevin.lamseck@gmail.com>
2019-11-20 12:37:54 +0100

    navbar

---
+++ app/javascript/plugins/sidelog.js
@@ -0,0 +1,12 @@
+// USER SIDE LOGIN
+
+function openNav() {
+  document.getElementById("mySidenav").style.width = "340px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+
+export { openNav, closeNav };
+
