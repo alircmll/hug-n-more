c68a79715ae49e2060b473a8c0dcfd1722b07284
Florent GONON <florent.gonon33@gmail.com>
2019-11-21 15:27:58 +0100

    J'ai rajouté des calins dans le seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,18 +14,19 @@ puts "Creating users..."
     password:     'azerty',
     hugger:        false)
 
- b = User.create!( username:     'Flopaille',
+ b = User.create!(
+    username:     'Flopaille',
     email:        'flo@gmail.com',
     address:      '3 place amédée larrieu,33000 Bordeaux',
     password:     'azerty',
     hugger:        false)
-c = User.create!(
+  c = User.create!(
     username:     'Lampoulpe',
     email:        'lam@gmail.com',
     address:      '7 Rue des Bahutiers,33000 Bordeaux',
     password:     'azerty',
     hugger:        true)
-d = User.create!(
+  d = User.create!(
     username:     'Xdel',
     email:        'xd@gmail.com',
     address:      '30 Rue du Pas-Saint-Georges,33000 Bordeaux',
@@ -42,62 +43,99 @@ hugs_attributes = [
     title:         'Calin au coin du feu',
     category:      3,
     photo:         "https://images.unsplash.com/photo-1517346665566-17b938c7f3ad?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
-    user:         a
+    user:          a
   },
   {
     description:  'Je ne mord pas et ne griffe pas non plus ',
     price:        154,
     title:        'Calin chaud sous le plaid',
     category:     2,
-    user_id:     'azerty',
+    user_id:      'azerty',
     photo:        "https://images.unsplash.com/photo-1517346665566-17b938c7f3ad?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
-    user:        b
+    user:         b
   },
   {
-    description:   "Je n'ai jamais pu refuser quoi que ce soit à une brune aux yeux marrons",
+    description:  "Je n'ai jamais pu refuser quoi que ce soit à une brune aux yeux marrons",
     price:        10,
     title:        'Calin reconfortant',
     category:     1,
-    user_id:     'azerty',
+    user_id:      'azerty',
     photo:        "https://images.unsplash.com/photo-1517346665566-17b938c7f3ad?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
-    user:      c
+    user:         c
   },
   {
     description:  "Satisfait ou remboursé",
     price:        56,
     title:        'Calin desinteressé',
     category:     0,
-    user_id:     'azerty',
-    photo:       "https://images.unsplash.com/photo-1517346665566-17b938c7f3ad?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
-    user:       d
+    user_id:      'azerty',
+    photo:        "https://images.unsplash.com/photo-1517346665566-17b938c7f3ad?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
+    user:         d
   },
   {
-    description:   "Idéale pour une étape sur la route des vacances.",
+    description:  "Idéale pour une étape sur la route des vacances.",
     price:        96,
     title:        "Calin d'escale",
     category:     3,
-    user_id:     'azerty',
+    user_id:      'azerty',
     photo:        "https://images.unsplash.com/photo-1517346665566-17b938c7f3ad?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
-    user:       c
+    user:         c
   },
   {
     description:  "Idéal pour un couple ou voyageur solo, vous pourrez profiter du patio indépendant au calme tout en étant proche des commerces.",
     price:        40,
-    title:      'Calin au coin de la rue',
+    title:        'Calin au coin de la rue',
     category:     2,
-    user_id:     'azerty',
+    user_id:      'azerty',
     photo:        "https://images.unsplash.com/photo-1517346665566-17b938c7f3ad?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
-    user:      a
+    user:         a
   },
   {
     description:   "Entièrement rénovée avec tout le confort et la modernité",
     price:         25,
-    title:        'Calin gourmand',
-    category:     1,
-    user_id:     'azerty',
-    photo:        "https://images.unsplash.com/photo-1517346665566-17b938c7f3ad?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
-    user:       b
+    title:         'Calin gourmand',
+    category:      1,
+    user_id:       'azerty',
+    photo:         "https://images.unsplash.com/photo-1517346665566-17b938c7f3ad?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
+    user:          b
+  },
+  {
+    description:   "Calin si affinité",
+    price:         25,
+    title:         'Calin poussé',
+    category:      3,
+    user_id:       'azerty',
+    photo:         "https://images.unsplash.com/photo-1517346665566-17b938c7f3ad?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
+    user:          b
+  },
+  {
+    description:   "Calin doux mais rapide",
+    price:         25,
+    title:         'Calin efficace',
+    category:      2,
+    user_id:       'azerty',
+    photo:         "https://images.unsplash.com/photo-1517346665566-17b938c7f3ad?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
+    user:          c
+  },
+  {
+    description:   "Calin dès l'aube",
+    price:         25,
+    title:         'Calin matinal',
+    category:      0,
+    user_id:       'azerty',
+    photo:         "https://images.unsplash.com/photo-1517346665566-17b938c7f3ad?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
+    user:          d
+  },
+  {
+    description:   "Calin sous pression",
+    price:         25,
+    title:         'Calin pas de tout repos',
+    category:      0,
+    user_id:       'azerty',
+    photo:         "https://images.unsplash.com/photo-1517346665566-17b938c7f3ad?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
+    user:          a
   }
 ]
+
 Hug.create!(hugs_attributes)
 puts 'Finished!'
